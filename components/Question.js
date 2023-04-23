ccb825071b525630280f739427115f1687d1a13d
Benjamin Rodenstein <benrodprod@gmail.com>
2023-04-23 09:27:36 +0200

    dummy data and a bit styling

--- components/Question.js
+++ components/Question.js
@@ -1,8 +1,18 @@
-export default function Question() {
+import styled from "styled-components";
+const StyledQuestion = styled.div`
+  display: flexbox;
+  border-radius: 5%;
+  justify-content: center;
+  height: 400px;
+  width: 100%;
+  background-color: yellow;
+  padding: 2rem;
+`;
+export default function Question({ question }) {
   return (
     <>
       <h1>Question</h1>
-      <p>lirum larum?</p>
+      <StyledQuestion>{question}</StyledQuestion>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Question from "@/components/Question";
 import Answer from "@/components/Answer";
+const library = [
+  {
+    question: "Wie lautet die Übersetzung für Huhn",
+    answer: "chicken",
+    category: "food",
+    benefit: "health",
+  },
+];
+console.log(library);
 export default function HomePage() {
   return (
     <div>
-      <Question />
-      <Answer />
+      <Question question={library[0].question} />
+      <Answer answer={library[0].answer} />
     </div>
   );
 }

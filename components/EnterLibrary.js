c9d3d37234f301df9f94680a073e9caef2f39521
Benjamin Rodenstein <benrodprod@gmail.com>
2023-04-27 12:13:39 +0200

    new navigation style

---
+++ components/EnterLibrary.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledImage = styled(Image)`
+  position: absolute;
+  top: 0rem;
+  left: 0;
+  object-fit: contain;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export default function EnterLibrary({ insideLibrary, toggleInsideLibrary }) {
+  console.log(insideLibrary);
+  return (
+    <>
+      {insideLibrary ? (
+        <Link href="/">
+          <StyledImage
+            onClick={toggleInsideLibrary}
+            src="/assets/journey.png"
+            alt="Library Icon"
+            height="80"
+            width="80"
+          ></StyledImage>
+        </Link>
+      ) : (
+        <Link href="/Library/">
+          <StyledImage
+            onClick={toggleInsideLibrary}
+            src="/assets/bookicon.png"
+            alt="Library Icon"
+            height="80"
+            width="80"
+          ></StyledImage>
+        </Link>
+      )}
+    </>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,21 @@
 import { useState } from "react";
-import Link from "next/link";
-import styled from "styled-components";
 import GlobalStyle from "../styles";
 import Head from "next/head";
 import { library } from "@/library/library";
-
-const StyledLink = styled(Link)`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  background-color: white;
-  color: black;
-  text-decoration: none;
-  width: 100%;
-`;
+import EnterLibrary from "@/components/EnterLibrary";
 
 export default function App({ Component, pageProps }) {
   const [currentLibrary, setCurrentLibrary] = useState(library);
+  const [insideLibrary, setInsideLibrary] = useState(false);
   function handleNewWisdomSubmit(wisdom) {
     setCurrentLibrary([...currentLibrary, wisdom]);
   }
 
+  function toggleInsideLibrary() {
+    setInsideLibrary(!insideLibrary);
+    ("toggle");
+  }
+  console.log("app_js", insideLibrary);
   return (
     <>
       <Head>
@@ -32,7 +27,11 @@ export default function App({ Component, pageProps }) {
         library={currentLibrary}
         handleNewWisdomSubmit={handleNewWisdomSubmit}
       />
-      <StyledLink href="/Library/">Add New Wisdom</StyledLink>
+
+      <EnterLibrary
+        insideLibrary={insideLibrary}
+        toggleInsideLibrary={toggleInsideLibrary}
+      />
     </>
   );
 }

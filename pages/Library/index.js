1b8e0a93952c3726da73e6097174bcef8af3085b
Benjamin Rodenstein <benrodprod@gmail.com>
2023-04-27 12:49:23 +0200

    popup when add wisdom and input focus

--- pages/Library/index.js
+++ pages/Library/index.js
@@ -1,13 +1,25 @@
 import { useRouter } from "next/router";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
 
 const StyledForm = styled.form`
   display: grid;
   gap: 2rem;
   flex-direction: column;
 `;
+const StyledPopup = styled.div`
+  position: absolute;
+  border: 2px solid white;
+  top: 30rem;
+  left: 0;
+  text-align: center;
+  width: 100%;
+`;
 
 export default function AddWisdom({ handleNewWisdomSubmit }) {
+  const inputRef = useRef(null);
+  const [popupActive, setPopupActive] = useState(false);
   const router = useRouter();
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,14 +27,21 @@ export default function AddWisdom({ handleNewWisdomSubmit }) {
     const formData = new FormData(event.target);
     const wisdomData = Object.fromEntries(formData);
     handleNewWisdomSubmit({ ...wisdomData, answeredRight: 0 });
-    router.push("/");
+    setPopupActive(true);
+    setTimeout(() => {
+      setPopupActive(false);
+    }, 1500);
+    event.target.reset();
+    inputRef.current.focus();
   }
-
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <>
       <StyledForm onSubmit={(event) => handleSubmit(event)}>
         <label htmlFor="question">Enter Question:</label>
-        <input required name="question" type="text"></input>
+        <input ref={inputRef} required name="question" type="text"></input>
         <label htmlFor="answer">Enter Answer:</label>
         <input required name="answer" type="text"></input>
         <label htmlFor="category">Pick a Category:</label>
@@ -39,6 +58,7 @@ export default function AddWisdom({ handleNewWisdomSubmit }) {
         </select>
         <button type="submit">submit</button>
       </StyledForm>
+      {popupActive ? <StyledPopup>Wisdom Added</StyledPopup> : null}
     </>
   );
 }

f68639d9a79e2bb344652939a91f6205c80d4bee
Benjamin Rodenstein <benrodprod@gmail.com>
2023-04-28 15:27:48 +0200

    added library

--- pages/library/index.js
+++ pages/library/index.js
@@ -1,112 +1,74 @@
-import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
+import InsertBook from "@/components/InsertBook";
 
-const BookPageStyle = styled.ul`
-  position: relative;
-  font-family: Georgia, "Times New Roman", Times, serif;
-  color: black;
-  list-style: none;
-
-  padding-left: 0;
-  margin-top: 5rem;
-  margin-bottom: 5rem;
-  margin-left: 3rem;
-  margin-right: 3rem;
-
-  li {
-    position: relative;
-    margin-top: 1rem;
-    margin-bottom: 2rem;
-  }
-
-  ::first-letter {
-    font-size: 3rem;
-    font-weight: bold;
-  }
+const BookShelfImage = styled(Image)`
+  position: flex;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
 `;
-const BookPageContainer = styled.div`
-  height: calc(100vh - 8rem);
-  margin-top: 3rem;
-  overflow: scroll;
+
+const AddNewBookButton = styled.button`
+  position: fixed;
+  bottom: 1rem;
+  left: 8rem;
+  width: 7rem;
 `;
-const FeatherLink = styled(Link)`
+
+const AddNewBookTitleInput = styled.input`
   position: fixed;
   bottom: 0;
+  left: 6.7rem;
 `;
-const StyledWisdom = styled.li`
-  position: relative;
 
-  li {
-    margin-top: 2rem;
-    margin-bottom: 2rem;
+export default function ViewLibrary({ library, setCurrentBook }) {
+  const [inputPopupActive, setInputPopupActive] = useState(false);
+
+  const router = useRouter();
+  function handleNewBookSubmit(event) {
+    event.preventDefault();
+
+    setCurrentBook(event.target.title.value);
+    router.push("/library/viewBook");
   }
-`;
-const BookPageImage = styled.div`
-  position: relative;
-  z-index: -2;
-  height: 100%;
-
-  &::before {
-    content: "";
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-image: url("/assets/bookpage.png");
-
-    background-size: cover;
-    opacity: 0.7;
-    z-index: -1;
-  }
-`;
-export default function ViewBook({ library }) {
-  const categories = library
-    .map((wisdom) => wisdom.category)
+  const books = library
+    .map((wisdom) => wisdom.book)
     .filter((value, index, self) => self.indexOf(value) === index);
 
   return (
     <>
-      <BookPageContainer>
-        <BookPageImage>
-          {categories.map((cat, index) => {
-            return (
-              <BookPageStyle key={cat}>
-                <li>
-                  <h1>
-                    Chapter {index + 1}: {cat}
-                  </h1>
-                </li>
-
-                {library
-                  .filter((wisdom) => wisdom.category === cat)
-                  .map((wisdom) => (
-                    <StyledWisdom key={wisdom.id}>
-                      <h2>Question:</h2>
-                      <p>{wisdom.question}</p>
-
-                      <h2>Answer:</h2>
-                      <p>{wisdom.answer}</p>
-
-                      <p>_____________________________</p>
-                    </StyledWisdom>
-                  ))}
-              </BookPageStyle>
-            );
-          })}
-        </BookPageImage>
-      </BookPageContainer>
-      <FeatherLink href="library/add" passHref legacyBehavior>
-        <Image
-          src="/assets/feather.png"
-          alt="feather"
-          width="150"
-          height="40"
-        ></Image>
-      </FeatherLink>
-      <p>You have {library.length} wisdoms in your book</p>
+      <BookShelfImage
+        src="/assets/bookshelf2.png"
+        height="1920"
+        width="1080"
+        alt="bookshelf"
+      ></BookShelfImage>
+      {books.map((book, index) => (
+        <InsertBook
+          key={book}
+          setCurrentBook={setCurrentBook}
+          bookName={book}
+          index={index}
+        ></InsertBook>
+      ))}
+      {books.length <= 6 ? (
+        <AddNewBookButton
+          onClick={() => setInputPopupActive(true)}
+          type="button"
+        >
+          Add new Book
+        </AddNewBookButton>
+      ) : null}
+      {inputPopupActive && (
+        <form onSubmit={handleNewBookSubmit}>
+          <AddNewBookTitleInput required name="title"></AddNewBookTitleInput>
+          <AddNewBookButton type="submit">Submit</AddNewBookButton>
+        </form>
+      )}
     </>
   );
 }
